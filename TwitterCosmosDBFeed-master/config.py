c056d008e63c7890b495946ea5b4ba1921731350
Adrián Del Rincón López <arincon@encamina.com>
2018-04-25 16:55:14 +0200

    Initial commit of session resources

---
+++ TwitterCosmosDBFeed-master/config.py
@@ -0,0 +1,15 @@
+# Enter twitter OAuth keys here.
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+# Enter CosmosDB config details below.
+#	Enter your masterKey, endpoint here.
+masterKey = '' 
+host = ''
+
+#	Enter your database, collection and preferredLocations here.
+databaseId = ''
+collectionId = ''
+preferredLocations = ''
